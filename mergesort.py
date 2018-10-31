9f675a656a9cb49a83e651f0824c66a2bc59457a
Sidhu177 <sidharth177@gmail.com>
2018-10-30 21:53:13 -0400

    mergesort algorithm

---
+++ mergesort.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 29 22:10:35 2018
+
+Taken from Data Structures and Algorithms using Python
+
+@author: SIDHARTH
+"""
+
+def merge_sort(S):
+    n = len(S)
+    if n<2:
+        return 
+    mid = n//2
+    S1 = S[0:mid]
+    S2 = S[mid:n]
+    merge_sort(S1)
+    merge_sort(S2)
+    merge(S1,S2,S)
