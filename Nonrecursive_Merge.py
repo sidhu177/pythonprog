30f31afc022b927d51fb68df0ed354970aecb85b
Sidhu177 <sidharth177@gmail.com>
2018-11-01 22:38:11 -0400

    Non recursive Merge sort

---
+++ Nonrecursive_Merge.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 22:26:58 2018
+
+Taken from Data Structures and Algorithms using Python
+
+@author: SIDHARTH
+"""
+
+def merge(src, result, start, inc):
+    end1 = start + inc
+    end2 = min(start+2*inc, len(src))
+    x,y,z = start, start+inc, start
+    while x<end1 and y<end2:
+        if src[x] < src[y]:
+            result[z] = src[x]; x +=1
+        else:
+            result[z] = src[y]; y +=1
+        z +=1
+    if x < end1:
+        result[z:end2] = src[x:end1]
+    elif y < end2:
+        result[z:end2] = src[y:end2]
+        
+def merge_sort(S):
+    n = len(S)
+    logn = math.ceil(math.log(n,2))
+    src, dest = S, [None]*n
+    for i in (2**k for k in range(logn)):
+        for j in range(0, n, 2*i):
+            merge(src, dest, j,i)
+        src, dest = dest, src
+    if S is not src:
+        S[0:n] = src[0:n]
+        
+        
+    
