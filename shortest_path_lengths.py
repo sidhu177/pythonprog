0d0ec209689a420420aa68012ee01dec9d53c205
Sidhu177 <sidharth177@gmail.com>
2018-12-06 21:15:14 -0500

    Shortest Path Length

---
+++ shortest_path_lengths.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 21:09:23 2018
+
+Taken from Data Structures and Algorithms using Python
+
+@author: SIDHARTH
+"""
+
+def shortest_path_lengths(g,src):
+    d = {}
+    cloud = {}
+    pq = AdaptableHeapPriorityQueue()
+    pqlocator = {}
+    
+    for v in g.vertices():
+        if v is src:
+            d[v]=0
+        else:
+            d[v] = float('inf')
+        pqlocator[v] = pq.add(d[v],v)
+        
+    while not pq.is_empty():
+        key, u = pq.remove_min()
+        cloud[u]=key
+        del pqlocator[u]
+        for e in g.incident_edges(u):
+            v = e.opposite(u)
+            if v not in cloud:
+                wgt = e.element()
+                if d[u] + wgt<d[v]:
+                    d[u] = d[u]+wgt
+                    pq.update(pqlocator[v],d[v],v)
+                    
