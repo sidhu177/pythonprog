bd585b592fc3a31066f38bc07e60464042d5e4aa
sidhu177 <sidharth177@gmail.com>
2019-01-06 23:59:31 -0500

    example

---
+++ genomesort.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  6 23:48:22 2019
+
+gnomesort example from Python Algorithms by Magnus Lie Hetland
+
+@author: sidra
+"""
+
+def gnomesort(seq):
+    i =0 
+    while i < len(seq):
+        if i == 0 or seq[i-1] <= seq[i]:
+            i += 1
+        else:
+            seq[i], seq[i-1] = seq[i-1], seq[i]
+            i -= 1
