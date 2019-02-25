2641c60dae2e040dc4c1c9acb3c43f7438e74f26
sidhu177 <sidharth177@gmail.com>
2019-02-25 00:04:32 -0500

    integration

---
+++ Integrator.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 24 23:49:46 2019
+
+
+Functions for Integration
+Taken from the book "A primer on Scientific Programming using Python by Springer"
+
+"""
+
+class Integrator:
+    def __init__(self,a,b,n):
+        self.a, self.b, self.n=a,b,n
+        self.points, self.weights = self.construct_method()
+        
+    def construct_method(self):
+        raise NotImplementedError('no rule in class %s' %self.__class__.__name__)
+        
+    def integrate(self,f):
+        s = 0
+        for i in range(len(self.weights)):
+            s += self.weights[i]*f(self.points[i])
+        return s
